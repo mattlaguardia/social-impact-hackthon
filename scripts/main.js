c533927f4dc9098d8867a2da1e1de6cf11579bd3
byfyang <darkspherico@gmail.com>
2016-08-20 14:56:10 -0700

    added dom manipulation

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,7 @@
+console.log("connected");
+var x;
 $(document).ready(function(){
-  var x;
+  
   $.ajax({
     url: "http://www.buzzfeed.com/api/v2/feeds/news",
     context: document.body
@@ -8,6 +10,15 @@ $(document).ready(function(){
     console.log(x);
   });
 
+  // getTitle();
 
 });
 
+
+var getTitle = function(){
+  for(i=0;i<=6;i++){
+    var title = x.big_stories[i].content_items[0].title;
+    var newDiv = document.createElement("div");
+    newDiv.appendChild(title);
+  }
+};
